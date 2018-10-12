ab91208f9155eaa4e5d0f64fce92b483da66dd44
jankabatek <jan.kabatek@gmail.com>
2018-10-12 04:45:13 +0100

    update

---
+++ library_subref.py
@@ -0,0 +1,17 @@
+### mainprogam.py
+### IMPORTS ANOTHER MODULE
+# from lib import disp
+from lib import misc
+
+### USING AN IMPORTED MODULE
+# Use the form modulename.itemname
+# Examples:
+
+
+print misc.NRONE
+
+# moduletest.timesfour(4)
+
+# print moduletest.ageofqueen
+# cfcpiano = moduletest.Piano()
+# cfcpiano.printdetails()
